9cdf5a20ff212cf0cc6dd29720e9419de6bbd333
RajithaKWG <rkrishan894@gmail.com>
2024-10-25 13:07:17 +0530

    verfied routing

--- demo-app/src/App.jsx
+++ demo-app/src/App.jsx
@@ -1,23 +1,20 @@
-import { Route, Routes } from "react-router-dom"
-import Home from '././Home'
-import Create from './components/create/Create'
-import Read from './components/read/Read'
-import Update from './components/update/Update'
-import Delete from './components/delete/Delete'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "././Home";
+import Create from "./components/create/Create";
+import Read from "./components/read/Read";
+import Update from "./components/update/Update";
+import Delete from "./components/delete/Delete";
 
 function App() {
-
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home/>}  />
-        <Route path="/create" element={<Create/>}  />
-        <Route path="/read" element={<Read/>}  />
-        <Route path="/update" element={<Update/>}  />
-        <Route path="/delete" element={<Delete/>}  />
-      </Routes>
-    </>
-  )
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/read/:id" element={<Read />} />
+          <Route path="/update/:id" element={<Update />} />
+          <Route path="/delete/:id" element={<Delete />} />
+        </Routes>
+  );
 }
 
-export default App
+export default App;
