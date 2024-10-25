ae91f30ed1f095b377ca785f58d649db0656e016
RajithaKWG <rkrishan894@gmail.com>
2024-10-25 14:22:14 +0530

    Implemented Create

--- demo-app/src/Home.jsx
+++ demo-app/src/Home.jsx
@@ -1,49 +1,55 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3000/users')
-    .then(response => setData(response.data))
-    .catch(err => console.log(err))
+    axios
+      .get("http://localhost:3000/users")
+      .then((response) => setData(response.data))
+      .catch((err) => console.log(err));
   }, []);
 
   return (
-    <div className='d-flex flex-column justify-content-center align-item-center bg-light vh-100'>
+    <div className="d-flex flex-column justify-content-center align-item-center bg-light vh-100">
       <h1>List of Users</h1>
-      <div className='w-75 rounded bg-white border shadow p-4'>
-        <table>
+      <div className="w-75 rounded bg-white border shadow p-4">
+        <div className="d-flex justify-content-end">
+          <Link to="/create" className="btn btn-success">
+            Add +
+          </Link>
+        </div>
+        <table className="table table-striped">
           <thead>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Action</th>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Action</th>
+            </tr>
           </thead>
           <tbody>
-            {
-              data.map((data, index) => (
-                <tr key={index}>
-                  <td>{data.id}</td>
-                  <td>{data.name}</td>
-                  <td>{data.email}</td>
-                  <td>{data.phone}</td>
-                  <td>
-                    <button className='btn btn-sm btn-primary me-2'>Edit</button>
-                    <button className='btn btn-sm btn-danger'>Delete</button>
-                  </td>
-                </tr>
-              ))
-            }
+            {data.map((data, index) => (
+              <tr key={index}>
+                <td>{data.id}</td>
+                <td>{data.name}</td>
+                <td>{data.email}</td>
+                <td>{data.phone}</td>
+                <td>
+                  <button className="btn btn-sm btn-info me-2">Read</button>
+                  <button className="btn btn-sm btn-primary me-2">Edit</button>
+                  <button className="btn btn-sm btn-danger">Delete</button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
 export default Home;
